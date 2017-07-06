e1a417b3fa5b5be4da126d06eb78e157b722659c
sabin <nibaserc@gmail.com>
2017-07-05 22:23:44 -0500

    Annotation added in Person Repository

--- src/main/java/com/webservice/test/restfulWebserviceTesting/Repository/PersonRepository.java
+++ src/main/java/com/webservice/test/restfulWebserviceTesting/Repository/PersonRepository.java
@@ -1,9 +1,11 @@
 package com.webservice.test.restfulWebserviceTesting.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.webservice.test.restfulWebserviceTesting.Model.Person;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
