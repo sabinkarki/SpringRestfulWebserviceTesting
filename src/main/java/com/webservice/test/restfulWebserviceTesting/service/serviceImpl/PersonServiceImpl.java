05cc68ab4916caf6649a60eed3498424999810ed
sabin <nibaserc@gmail.com>
2017-07-05 22:58:24 -0500

    @Transcation added to the person service

--- src/main/java/com/webservice/test/restfulWebserviceTesting/service/serviceImpl/PersonServiceImpl.java
+++ src/main/java/com/webservice/test/restfulWebserviceTesting/service/serviceImpl/PersonServiceImpl.java
@@ -2,12 +2,17 @@ package com.webservice.test.restfulWebserviceTesting.service.serviceImpl;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.webservice.test.restfulWebserviceTesting.Model.Person;
 import com.webservice.test.restfulWebserviceTesting.Repository.PersonRepository;
 import com.webservice.test.restfulWebserviceTesting.service.PersonService;
 
+@Service
+@Transactional
 public class PersonServiceImpl implements PersonService {
 
 	@Autowired
