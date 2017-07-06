8b00fd86168f62fb6ee334c0222538cc07f968d4
sabin <nibaserc@gmail.com>
2017-07-06 00:23:47 -0500

    Wrapper object added for response entity

---
+++ src/main/java/com/webservice/test/restfulWebserviceTesting/response/Response.java
@@ -0,0 +1,36 @@
+package com.webservice.test.restfulWebserviceTesting.response;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Response {
+	private int value;
+	private String message;
+
+	public Response() {
+	}
+
+	public Response(int value, String message) {
+		super();
+		this.value = value;
+		this.message = message;
+
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
