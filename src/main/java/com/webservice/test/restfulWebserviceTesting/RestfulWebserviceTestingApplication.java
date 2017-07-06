d4acb7b84964964985231c69147b9f4da99eb830
sabin <nibaserc@gmail.com>
2017-07-05 23:41:33 -0500

    CommandLineRunner bean added in main class

--- src/main/java/com/webservice/test/restfulWebserviceTesting/RestfulWebserviceTestingApplication.java
+++ src/main/java/com/webservice/test/restfulWebserviceTesting/RestfulWebserviceTestingApplication.java
@@ -1,12 +1,43 @@
 package com.webservice.test.restfulWebserviceTesting;
 
+/**
+ @author sabin
+ */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.webservice.test.restfulWebserviceTesting.Model.Address;
+import com.webservice.test.restfulWebserviceTesting.Model.Person;
+import com.webservice.test.restfulWebserviceTesting.Repository.PersonRepository;
 
 @SpringBootApplication
 public class RestfulWebserviceTestingApplication {
+	private static final Logger logger = LoggerFactory.getLogger(RestfulWebserviceTestingApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebserviceTestingApplication.class, args);
+	}
+
+	/*
+	 * When you want to execute some piece of code exactly before the
+	 * application startup completes, you can use it then
+	 */
+	@Bean
+	public CommandLineRunner setUp(PersonRepository personRepository) {
+		return (args) -> {
+			personRepository.save(new Person("Sabin", "Software Developer",
+					new Address("MeadowCreek Drive", "1110", "Irving Texas", "12345")));
+			personRepository
+					.save(new Person("Nikita", "Lawyer", new Address("Dallas Drive", "1269", "Dallas Texas", "78569")));
+			personRepository.save(
+					new Person("Jack", "DBA", new Address("Washington PKWY", "8914", "Maclean Virgina ", "89567")));
+			personRepository.save(new Person("Baklol", " Software Tester",
+					new Address("Jonh F Carpenter Street", "1550", "Las-Colinas Texas", "10256")));
+			logger.info("Sample Data Generated");
+		};
 	}
 }
